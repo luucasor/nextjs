01f30f8f922952832bcdac891f7390e22df704d5
luucasor <lucas@jornadatecnologia.com>
2022-10-14 15:19:25 -0300

    Primeiras páginas

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>Erro 404</h1>
+            <Link href="/">Ir para Home</Link>
+        </div>
+    );
+}
